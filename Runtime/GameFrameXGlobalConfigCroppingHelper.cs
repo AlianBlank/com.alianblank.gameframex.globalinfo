334429d9089b03f5260d1622cbe8691d1b3da9c6
AlianBlank <wangfj11@foxmail.com>
2024-04-10 18:00:31 +0800

    [增加]1. 增加防裁剪标记

--- Runtime/GameFrameXGlobalConfigCroppingHelper.cs
+++ Runtime/GameFrameXGlobalConfigCroppingHelper.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace GameFrameX.GlobalConfig.Runtime
 {
+    [Preserve]
     public class GameFrameXGlobalConfigCroppingHelper : MonoBehaviour
     {
+        [Preserve]
         private void Start()
         {
             _ = typeof(GlobalConfigComponent);
